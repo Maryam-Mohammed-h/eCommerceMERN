16c71c9b51bb49a578c0388f1fcbe8000b52b473
Maryam-Mohammed-h <webdevelopmentservices2022@gmail.com>
2024-07-31 03:43:09 +0200

    edit order  cart function

--- src/modules/Orders/order.controller.js
+++ src/modules/Orders/order.controller.js
@@ -215,7 +215,7 @@ export const fromCartToOrder = async (req, res, next) => {
   const userId = req.authUser._id;
   // const { cartId } = req.params;
   const { address, phoneNumber, paymentMethod, couponCode } = req.body;
-  const cart = await cartModel.findById({ userId });
+  const cart = await cartModel.findOne({ userId });
   if (!cart || !cart.products.length) {
     return next(
       new Error(`Cart has no items yet`, {
